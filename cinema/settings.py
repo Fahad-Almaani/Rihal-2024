7059745b3772e179b804706f4aba2548fe0a602a
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2024-04-08 22:08:21 +0400

    connecting elelephantsql

--- cinema/settings.py
+++ cinema/settings.py
@@ -80,10 +80,20 @@ WSGI_APPLICATION = 'cinema.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qsmdanre',
+        'USER': 'qsmdanre',
+        'PASSWORD': '0A642a3cEF-LrthhTfWHNRmt0XNMAWyi',
+        'HOST': 'flora.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
